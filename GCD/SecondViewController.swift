38628b2338dad3fc22e329a38b4f1e7ee3fb240f
Dmitry Komissarov <komdimkom@gmail.com>
2023-06-27 11:31:10 +0300

    Create View and SecondView with ImageView

---
+++ GCD/SecondViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SecondViewController.swift
+//  GCD
+//
+//  Created by mac on 27.06.2023.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
